a865d6fc34be0a576feea343dc7508fe6c456794
flightcoded <37447279+flightcoded@users.noreply.github.com>
2020-07-01 15:01:52 +0300

    revised code

--- GraphicsActivity.java
+++ GraphicsActivity.java
@@ -1,34 +1,28 @@
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.opengl.GLSurfaceView;
-import android.content.Context;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 
 public class GraphicsActivity extends AppCompatActivity{
 
-  private GLSurfaceView gLView;
+  private GLSurfaceView glView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState){
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_graphics);
-    
-    gLView = new SurfaceView(this);
-    setContentView(gLView);
 
-    findViewById(android.R.id.content).getRootView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    if(Build.VERSION.SDK_INT >= 28){
+    glView = new SurfaceView(this);
+    setContentView(glView); //setting our renderer as the view
+
+    findViewById(android.R.id.content).getRootView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION); //fullscreen
+    if(Build.VERSION.SDK_INT >= 28){ //to use the place of the status bar on devices with a cutout
 
       WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
       layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
@@ -43,7 +37,7 @@ public class GraphicsActivity extends AppCompatActivity{
     super.onResume();
     findViewById(android.R.id.content).getRootView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
   }
-  
+
   class SurfaceView extends GLSurfaceView{
 
     flyRenderer renderer;
