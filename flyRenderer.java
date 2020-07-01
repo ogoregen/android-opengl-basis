29f3edfebfcbd7fa88c171eae950393f1c252c02
flightcoded <37447279+flightcoded@users.noreply.github.com>
2020-07-01 12:34:28 +0300

    created renderer class

---
+++ flyRenderer.java
@@ -0,0 +1,61 @@
+
+import android.opengl.GLES30;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.content.Context;
+import android.util.Log;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class flyRenderer implements GLSurfaceView.Renderer{
+
+  Context context;
+  int width, height;
+  Shader shader;
+  float[] projection = new float[16];
+  float[] view = new float[16];
+  float[] model = new float[16];
+  
+  flyRenderer(int width_, int height_, Context context_){
+    
+    width = width_;
+    height = height_;
+    context = context_;
+  }
+
+  public void onSurfaceCreated(GL10 unused, EGLConfig config){
+
+    GLES30.glEnable(GLES30.GL_DEPTH_TEST);
+    GLES30.glEnable(GLES30.GL_BLEND);
+    GLES30.glBlendFunc(GL10.GL_ONE, GL10.GL_ONE_MINUS_SRC_ALPHA);
+    GLES30.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    
+    shader = new Shader();
+    shader.bind();
+    
+    shader.setUniform("usingTexture", true);
+    float[] fillColor = {1.0f, 1.0f, 1.0f, 1.0f};
+    shader.setUniformV("color", fillColor);
+
+    Matrix.setIdentityM(projection, 0);
+    Matrix.setIdentityM(view, 0);
+    Matrix.setIdentityM(model, 0);
+    shader.setUniformM("projection", projection);
+    shader.setUniformM("view", view);
+    shader.setUniformM("model", model);
+  }
+
+  public void onDrawFrame(GL10 unused){
+
+    GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT | GLES30.GL_DEPTH_BUFFER_BIT);
+
+    //draw calls here
+  }
+
+  public void onSurfaceChanged(GL10 unused, int width, int height){
+
+    GLES30.glViewport(0, 0, width, height);
+    Matrix.orthoM(projection, 0, 0, width, height, 0, -10, 10);
+    shader.setUniformM("projection", projection);
+  }
+}
