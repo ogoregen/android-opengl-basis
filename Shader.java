0f5550ad7eee674b37ae75630249641f7709c545
flightcoded <37447279+flightcoded@users.noreply.github.com>
2020-07-05 21:45:00 +0300

    made GLES30 import static

--- Shader.java
+++ Shader.java
@@ -1,5 +1,5 @@
 
-import android.opengl.GLES30;
+import static android.opengl.GLES30.*;
 
 class Shader{
 
@@ -31,51 +31,51 @@ class Shader{
       "  else gl_FragColor = color;" +
       "}";
 
-    int vs = compileShader(GLES30.GL_VERTEX_SHADER, vsCode);
-    int fs = compileShader(GLES30.GL_FRAGMENT_SHADER, fsCode);
-    id = GLES30.glCreateProgram();
-    GLES30.glAttachShader(id, vs);
-    GLES30.glAttachShader(id, fs);
-    GLES30.glLinkProgram(id);
-    GLES30.glDeleteProgram(vs);
-    GLES30.glDeleteProgram(fs);
+    int vs = compileShader(GL_VERTEX_SHADER, vsCode);
+    int fs = compileShader(GL_FRAGMENT_SHADER, fsCode);
+    id =  glCreateProgram();
+    glAttachShader(id, vs);
+    glAttachShader(id, fs);
+    glLinkProgram(id);
+    glDeleteProgram(vs);
+    glDeleteProgram(fs);
   }
 
   private int compileShader(int type, String shaderCode){
 
-    int shader = GLES30.glCreateShader(type);
-    GLES30.glShaderSource(shader, shaderCode);
-    GLES30.glCompileShader(shader);
+    int shader = glCreateShader(type);
+    glShaderSource(shader, shaderCode);
+    glCompileShader(shader);
     return shader;
   }
 
   void bind(){
 
-    GLES30.glUseProgram(id);
+    glUseProgram(id);
   }
   
   static void unbind(){
     
-    GLES30.glUseProgram(0);
+    glUseProgram(0);
   }
 
   int getAttributeLocation(String name){
 
-    return GLES30.glGetAttribLocation(id, name);
+    return glGetAttribLocation(id, name);
   }
 
   void setUniformV(String name, float[] value){
 
-    GLES30.glUniform4fv(GLES30.glGetUniformLocation(id, name), 1, value, 0);
+    glUniform4fv(glGetUniformLocation(id, name), 1, value, 0);
   }
 
   void setUniformM(String name, float[] value){
 
-    GLES30.glUniformMatrix4fv(GLES30.glGetUniformLocation(id, name), 1, false, value, 0);
+    glUniformMatrix4fv(glGetUniformLocation(id, name), 1, false, value, 0);
   }
 
   void setUniform(String name, boolean value){
 
-    GLES30.glUniform1i(GLES30.glGetUniformLocation(id, name), value ? 1 : 0);
+    glUniform1i(glGetUniformLocation(id, name), value ? 1 : 0);
   }
 }
