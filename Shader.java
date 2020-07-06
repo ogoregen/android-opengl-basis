7c7992ae93c68cbccf125dff7663b3db775dbb64
flightcoded <37447279+flightcoded@users.noreply.github.com>
2020-07-06 15:18:22 +0300

    removed shader codes, added function to read shaders

--- Shader.java
+++ Shader.java
@@ -1,44 +1,46 @@
 
+import android.content.Context;
 import static android.opengl.GLES30.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 class Shader{
 
   private int id;
 
-  Shader(){
+  Shader(int vsResourceID, int fsResourceID, Context context){
 
-    String vsCode = "" +
-      "attribute vec3 position;" +
-      "attribute vec2 texturePosition;" +
-      "attribute vec3 instancePosition;" +
-      "varying vec2 textureP;" +
-      "uniform mat4 projection;" +
-      "uniform mat4 view;" +
-      "uniform mat4 model;" +
-      "void main(){" +
-      "  textureP = texturePosition;" +
-      "  gl_Position = projection * view * model * vec4(position + instancePosition, 1);" +
-      "}";
-
-    String fsCode = "" +
-      "precision mediump float;" +
-      "uniform vec4 color;" +
-      "uniform bool usingTexture;" +
-      "varying vec2 textureP;" +
-      "uniform sampler2D textureSampler;" +
-      "void main(){" +
-      "  if(usingTexture) gl_FragColor = texture2D(textureSampler, textureP);" +
-      "  else gl_FragColor = color;" +
-      "}";
-
-    int vs = compileShader(GL_VERTEX_SHADER, vsCode);
-    int fs = compileShader(GL_FRAGMENT_SHADER, fsCode);
-    id =  glCreateProgram();
+    int vs = compileShader(GL_VERTEX_SHADER, readShader(vsResourceID, context));
+    int fs = compileShader(GL_FRAGMENT_SHADER, readShader(fsResourceID, context));
+    id = glCreateProgram();
     glAttachShader(id, vs);
     glAttachShader(id, fs);
     glLinkProgram(id);
     glDeleteProgram(vs);
     glDeleteProgram(fs);
+  }
+
+  private String readShader(int resourceID, Context context){
+
+    InputStream rawResource = context.getResources().openRawResource(resourceID);
+    BufferedReader reader = new BufferedReader(new InputStreamReader(rawResource));
+    StringBuilder builder = new StringBuilder();
+    String line;
+
+    try{
+
+      while((line = reader.readLine()) != null) builder.append(line + "\n");
+    }
+    catch(Exception e){}
+
+    try{
+
+      rawResource.close();
+    }
+    catch(Exception e){}
+
+    return builder.toString();
   }
 
   private int compileShader(int type, String shaderCode){
@@ -52,11 +54,6 @@ class Shader{
   void bind(){
 
     glUseProgram(id);
-  }
-  
-  static void unbind(){
-    
-    glUseProgram(0);
   }
 
   int getAttributeLocation(String name){
